ec1912e0a2a2248ff7fe99a638e1045c47ed7fc3
Chris Dyer <cdyer1980@gmail.com>
2017-11-18 10:53:16 -0500

    Initial add of files.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,20 @@
+export default function() {
+    return [
+        {
+            title: 'Javascript the good parts',
+            pages: 101
+        },
+        {
+            title: 'The dark tower',
+            pages: 405
+        },
+        {
+            title: 'Gone with the wind',
+            pages: 1054
+        },
+        {
+            title: 'Jaws',
+            pages: 456
+        },
+    ]
+}
